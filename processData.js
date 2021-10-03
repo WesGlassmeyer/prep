90f053797dc7727b4db943377940bc2e34c24b36
Wes <mwg1825@aol.com>
2021-10-02 18:00:04 -0600

    Update processData.js

--- processData.js
+++ processData.js
@@ -1,34 +1,43 @@
 function processData(input) {
+  let input1 = input.split("\n");
+  let output = [];
   let arr = [];
-  if (input[0] === "S" && input[2] === "M") {
-    arr = input.substring(4).split(/(?=[A-Z])/);
-    arr[1] = arr[1].toLowerCase().substring(0, arr[1].length - 2);
-    console.log(arr[0] + " " + arr[1]);
-  }
-  if (input[0] === "S" && input[2] === "V") {
-    arr = input.substring(4).split(/(?=[A-Z])/);
-    arr[1] = arr[1].toLowerCase();
-    console.log(arr[0] + " " + arr[1]);
-  }
-  if (input[0] === "C" && input[2] === "M") {
-    arr = input.substring(4).split(" ");
-    arr[1] = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
-    console.log(arr[0] + arr[1] + "()");
-  }
-  if (input[0] === "C" && input[2] === "C") {
-    arr = input.substring(4).split(" ");
-    arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-    arr[1] = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
-    console.log(arr[0] + arr[1]);
-  }
-  if (input[0] === "S" && input[2] === "C") {
-    arr = input.substring(4).split(/(?=[A-Z])/);
-    arr[0] = arr[0].charAt(0).toLowerCase() + arr[0].slice(1);
-    arr[1] = arr[1].charAt(0).toLowerCase() + arr[1].slice(1);
-    console.log(arr[0] + " " + arr[1]);
-  }
+  input1.forEach((item) => {
+    if (item[0] === "S" && item[2] === "M") {
+      arr = item.substring(4).split(/(?=[A-Z])/);
+      arr[1] = arr[1].toLowerCase().substring(0, arr[1].length - 2);
+      output.push(arr[0] + " " + arr[1]);
+    }
+    if (item[0] === "S" && item[2] === "V") {
+      arr = item.substring(4).split(/(?=[A-Z])/);
+      arr[1] = arr[1].toLowerCase();
+      output.push(arr[0] + " " + arr[1]);
+    }
+    if (item[0] === "C" && item[2] === "M") {
+      arr = item.substring(4).split(" ");
+      arr[1] = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
+      output.push(arr[0] + arr[1] + "()");
+    }
+    if (item[0] === "C" && item[2] === "C") {
+      arr = item.substring(4).split(" ");
+      arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+      arr[1] = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
+      output.push(arr[0] + arr[1]);
+    }
+    if (item[0] === "S" && item[2] === "C") {
+      arr = item.substring(4).split(/(?=[A-Z])/);
+      arr[0] = arr[0].charAt(0).toLowerCase() + arr[0].slice(1);
+      arr[1] = arr[1].charAt(0).toLowerCase() + arr[1].slice(1);
+      output.push(arr[0] + " " + arr[1]);
+    }
+  });
+  output.forEach((item) => {
+    console.log(item);
+  });
 }
 
-console.log(processData("S;C;OrangeHighlighter"));
+console.log(
+  processData("S;V;iPad\nC;M;mouse pad\nC;C;code swarm\nS;C;OrangeHighlighter")
+);
 
 // https://www.hackerrank.com/challenges/three-month-preparation-kit-camel-case/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
